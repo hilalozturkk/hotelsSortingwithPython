1c74275fdfa916c8f8daa57e4746b3b6005a6e76
Hilal Ozturk <67756727+hilalozturkk@users.noreply.github.com>
2021-04-03 04:40:39 +0300

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,140 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+
+df = pd.read_csv('chennai_reviews.csv')
+df.head()
+
+
+liste = df["Hotel_name"]
+print(liste)
+
+
+
+hotels = []
+x=1
+while x<len(liste):
+    
+        if df["Hotel_name"].loc[x] != df["Hotel_name"].loc[x-1] :
+             hotels.append(df["Hotel_name"].loc[x-1])
+         
+        x += 1
+
+
+
+i=0
+for x in hotels:
+     print(i ,"\t", x, "\n")
+     i += 1
+
+
+
+#m=df["Review_Text"].isin(["good"])
+#df[m]
+#df = df['Review_Text'].values
+#df = df.reshape(-1,1)
+#print(df.shape)
+#df[:4724]
+#a= np.array(df)
+
+#print(a)
+
+
+
+
+m=df["Review_Text"]
+
+
+import re
+i = 0
+toplam = 0
+
+liste2 = df["Hotel_name"]
+liste3 = []
+for x in m :
+    text = x
+    search_great = "(Good?)|(good?)"
+    search_great_counter = 0
+
+    search_helpful = "(Helpful?)|(helpful?)"
+    search_helpful_counter = 0
+    
+    search_excellent = "(Excellent?)|(excellent?)"
+    search_excellent_counter = 0
+   
+    for match in re.finditer(search_great,text):
+        search_great_counter += 1
+        print(match)
+        print(search_great_counter)
+        print(liste2[i]) 
+        liste3.append(liste2[i]) 
+        toplam = toplam + 1
+        
+    for match in re.finditer(search_helpful,text):
+        search_helpful_counter += 1
+        print(match)
+        print(search_helpful_counter)  
+        print(liste2[i]) 
+        liste3.append(liste2[i]) 
+        toplam = toplam + 1
+    
+    for match in re.finditer(search_excellent,text):
+        search_excellent_counter += 1
+        print(match)
+        print(search_excellent_counter)
+        print(liste2[i]) 
+        liste3.append(liste2[i]) 
+        toplam = toplam + 1
+        
+    i = i+1   
+    
+
+
+print("positive commit = ")    
+print(toplam)
+
+
+
+i=0
+for x in liste3:
+        print(i)
+        print(liste3[i])
+        i = i+1
+
+
+
+
+from collections import Counter
+otelSayisi = []
+x=0
+while x<len(liste3):
+    
+        if liste3[x] != liste3[x-1] :
+             otelSayisi.append(liste3[x])
+             
+        x =x + 1
+        
+
+        
+i=0
+for x in otelSayisi:
+     print(i ,"\t", x, "\n")
+     i += 1
+        
+        
+        
+c = Counter(liste3)  
+dict(c) #array için elamandan kaç tane var       
+        
+    
+    
+    
+
+
+sorted(c)
+print(c)
+
